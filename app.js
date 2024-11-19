764b3444038746d0697540a22eee1c2dfef056e5
aayushajs <aayushj004@gmail.com>
2024-11-20 00:21:22 +0530

    This is My Hospital Management System

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";  //  This is showing  error to not axces to backend .
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+
+     
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO], // This is a occer the error in gat not sharching a path root in config.env
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+     
+  })
+);
+//hotoy59049@nozamas.com:-Gadha@1234
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
