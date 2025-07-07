3d3909b6be470c2e6bab44b07aaf75fda4af5237
Aayushajs <aayushj004@gmail.com>
2025-07-07 19:26:25 +0530

    improve api logic in loging api make fast response

--- controller/userController.js
+++ controller/userController.js
@@ -41,31 +41,42 @@ export const patientRegister = catchAsyncErrors(async (req, res, next) => {
   generateToken(user, "User Registered!", 200, res);
 });
 
-// login section
+
 export const login = catchAsyncErrors(async (req, res, next) => {
   const { email, password, confirmPassword, role } = req.body;
+
+
   if (!email || !password || !confirmPassword || !role) {
     return next(new ErrorHandler("Please Fill Full Form!", 400));
   }
+
+ 
   if (password !== confirmPassword) {
-    return next(
-      new ErrorHandler("Password & Confirm Password Do Not Match!", 400)
-    );
+    return next(new ErrorHandler("Password & Confirm Password Do Not Match!", 400));
   }
+
+  
   const user = await User.findOne({ email }).select("+password");
+
   if (!user) {
     return next(new ErrorHandler("Invalid Email Or Password!", 400));
   }
 
+  
+  if (role !== user.role) {
+    return next(new ErrorHandler(`User Not Found With This Role!`, 400));
+  }
+
+  
   const isPasswordMatch = await user.comparePassword(password);
   if (!isPasswordMatch) {
     return next(new ErrorHandler("Invalid Email Or Password!", 400));
   }
-  if (role !== user.role) {
-    return next(new ErrorHandler(`User Not Found With This Role!`, 400));
-  }
+
   generateToken(user, "Login Successfully!", 201, res);
 });
+
+
 // addmin section
 export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
   const { firstName, lastName, email, phone, nic, dob, gender, password } =
